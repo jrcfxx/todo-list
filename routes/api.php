9ccb3b9a6a9bffc314124bcab9e239d4b09ce775
fiorinij-risk <fiorinij@risk.regn.net>
2024-07-08 22:01:39 -0300

    Creation of the controllers and the routes of the api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskChangeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('roles', RoleController::class);
+Route::apiResource('permissions', PermissionController::class);
+Route::apiResource('tasks', TaskController::class);
+Route::apiResource('task-changes', TaskChangeController::class)->only(['index', 'show']);
