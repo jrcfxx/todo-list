8268f58a460ede05cc9dd03e38dd5f93dddf6d18
fiorinij-risk <fiorinij@risk.regn.net>
2024-07-11 16:09:26 -0300

    Resolving API Issues - Successful PUT Request for Users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -85,7 +85,7 @@ class UsersController extends Controller
     {
         $request->validate([
             'name' => 'sometimes|required',
-            'email' => 'sometimes|required|email|unique:users,email,' . $users->id, 
+            'email' => 'sometimes|required|email|unique:users,email,' . $user->id, 
             'password' => 'sometimes|required|min:6',
             'role_id' => 'sometimes|required|exists:role,id',
         ]);
